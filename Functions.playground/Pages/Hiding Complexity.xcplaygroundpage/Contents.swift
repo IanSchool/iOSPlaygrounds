515828c017f94c4cd937b4224f41d69fd0f76d21
Ian <ian@Administrators-iMac-95.local>
2020-02-03 14:05:56 -0500

    Finihsed Functions

---
+++ Functions.playground/Pages/Hiding Complexity.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+
+verseTwo()
+breatheBetweenVerses()
+verseThree()
+breatheBetweenVerses()
+verseOne()
+/*: 
+ - experiment: Move the lines around to sing the verses in a different order or copy lines to repeat a verse.
+
+ On the next page, learn about another benefit of functions - making your programs easier to change.
+ 
+ [Previous](@previous)  |  page 8 of 12  |  [Next: Change Something Once](@next)
+*/

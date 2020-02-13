d5c27bc718674f071a5be280288b63e307cec8fd
Ian <ian@Administrators-iMac-95.local>
2020-02-13 14:58:26 -0500

    Working on Traffic Lights

---
+++ WorkingTrafficLights/WorkingTrafficLights/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WorkingTrafficLights
+//
+//  Created by Ian on 2/13/20.
+//  Copyright © 2020 Ian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func returnToFirstScreen(unwindSegue: UIStoryboardSegue){
+        
+    }
+    
+}
+

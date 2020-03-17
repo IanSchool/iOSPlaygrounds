d9297cf1a5c86335c74fdbfff78c562893b9ae30
Ian Stedham <Ian@AdminisatorsAir.fios-router.home>
2020-03-17 14:48:24 -0400

    Fixed thread error and almost finished Lesson 8

--- Journal/Journal/JournalEntryViewController.swift
+++ Journal/Journal/JournalEntryViewController.swift
@@ -12,7 +12,7 @@
 
 import UIKit
 
-class JournalEntryViewController: UITableViewController {
+class JournalEntryViewController: UIViewController {
 
     @IBOutlet weak var journalEntryContents: UITextView!
     var journalEntry: JournalEntry?
@@ -33,15 +33,7 @@ class JournalEntryViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+    
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {

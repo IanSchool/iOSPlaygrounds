587398cc44ac0cb5ca4fc640648463f3e80a071d
Ian Stedham <Ian@AdminisatorsAir.fios-router.home>
2020-04-08 11:10:01 -0400

    Created final app, decided to go with single view app after research, found images for a deck of cards to use in game

---
+++ Fyre/Fyre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fyre
+//
+//  Created by Ian Stedham on 4/8/20.
+//  Copyright © 2020 Ian Stedham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

c11558ba6ea8cb3292237cad3f26333014f9c9ae
Ian Stedham <Ian@AdminisatorsAir.fios-router.home>
2020-05-05 11:43:45 -0400

    created PlayScreen class which is a class for the view controller the game is on, made connections into it, and did research on transfering info from one view controller to another and the prepare func

---
+++ Fyre/Fyre/PlayScreen.swift
@@ -0,0 +1,23 @@
+//
+//  PlayScreen.swift
+//  Fyre
+//
+//  Created by Ian Stedham on 5/5/20.
+//  Copyright © 2020 Ian Stedham. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayScreen {
+    @IBOutlet weak var card1: UIImageView!
+    @IBOutlet weak var card2: UIImageView!
+    @IBOutlet weak var card3: UIImageView!
+    @IBOutlet weak var card4: UIImageView!
+    @IBOutlet weak var card5: UIImageView!
+    @IBOutlet weak var card6: UIImageView!
+    @IBOutlet weak var card7: UIImageView!
+    @IBOutlet weak var card8: UIImageView!
+    
+    
+}
--- Fyre/Fyre/ViewController.swift
+++ Fyre/Fyre/ViewController.swift
@@ -12,14 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var startingGold: UITextField!
     @IBOutlet weak var numOfPlayers: UITextField!
-    @IBOutlet weak var card1: UIImageView!
-    @IBOutlet weak var card2: UIImageView!
-    @IBOutlet weak var card3: UIImageView!
-    @IBOutlet weak var card4: UIImageView!
-    @IBOutlet weak var card5: UIImageView!
-    @IBOutlet weak var card6: UIImageView!
-    @IBOutlet weak var card7: UIImageView!
-    @IBOutlet weak var card8: UIImageView!
+    
     var numOfPlayersValue: Int?
     var goldAtStart: Int?
     
@@ -28,17 +21,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
     @IBAction func startGame(_ sender: Any) {
         numOfPlayersValue = Int(numOfPlayers.text!)
         goldAtStart = Int(startingGold.text!)
         var game = Game()
         game.start(playerCount: numOfPlayersValue!, startingGold: goldAtStart!)
         print(game.personPlayer.cardsInHand[0].cardName)
-        card1.image = UIImage(named: game.personPlayer.cardsInHand[0].cardName)
+        //PlayScreen.card1.image = UIImage(named: game.personPlayer.cardsInHand[0].cardName)
         performSegue(withIdentifier: "startGame", sender: nil)
         
     }
-
 
 }
 
